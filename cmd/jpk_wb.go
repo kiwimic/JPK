a0fb17c35f0dc687a635897df48085d50a8261a1
kiwimic <michalsiwik@gmail.com>
2018-08-28 19:06:35 +0200

    Dodano obsługe JPK_WB

---
+++ cmd/jpk_wb.go
@@ -0,0 +1,85 @@
+package cmd
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"time"
+
+	"github.com/jpk/utils"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	faCmd.PersistentFlags().String("file", "", "file jpk to read.")
+	faCmd.PersistentFlags().String("dir", "C:/jpk", "dir to export result tables")
+}
+
+var wbCmd = &cobra.Command{
+	Use:   "wb",
+	Short: "convert jpk_wb.xml to csv tables exported to dir you choose",
+	//Long: `A Fast and Flexible Static Site Generator built with
+	//              love by spf13 and friends in Go.
+	//              Complete documentation is available at http://hugo.spf13.com`,
+	Run: func(cmd *cobra.Command, args []string) {
+
+		st := time.Now()
+		fmt.Println("Zaczynamy: ", st)
+		//FilePath := "C:\\jpk\\xml_duzy.xml"
+		//ExportDir := "C:\\jpk"
+		//FilePath to JPK file
+		FilePath := cmd.Flag("file").Value.String()
+		fmt.Println("File path: ", FilePath)
+		//ExportDir where you save files
+		ExportDir := cmd.Flag("dir").Value.String()
+		fmt.Println("export dir: ", ExportDir)
+		NumerRachunkuName := ExportDir + "\\" + "NumerRachunku.txt"
+		SaldaName := ExportDir + "\\" + "Salda.txt"
+		WyciagCtrlName := ExportDir + "\\" + "WyciagCtrl.txt"
+		WYciagWierszName := ExportDir + "\\" + "WyciagWiersz.txt"
+		NaglowekName := ExportDir + "\\" + "Naglowek.txt"
+
+		xmlFile, err := os.Open(FilePath)
+		// if we os.Open returns an error then handle it
+		if err != nil {
+			fmt.Println(err)
+		}
+		// defer the closing of our xmlFile so that we can parse it later on
+		defer xmlFile.Close()
+
+		// read our opened xmlFile as a byte array.
+		byteValue, _ := ioutil.ReadAll(xmlFile)
+		v2 := time.Now()
+		fmt.Println("Wczytano plik: ", v2)
+		var jpk_wb utils.JPK_WB
+		xml.Unmarshal(byteValue, &jpk_wb)
+		v3 := time.Now()
+		fmt.Println("Zakończono parsowanie: ", v3)
+		//str := createCSVFaktura(JPK_FA.Faktura)
+		jpk_wb.CreateCSVFromRowsWyciagWiersz(200, WYciagWierszName)
+		fmt.Println("Stworzono CSV WyciagWiersz: ", time.Now())
+		//exportCSV(csvFaktura, "JPK_FA_Faktura.txt")
+
+		jpk_wb.CreateCSVSalda(SaldaName)
+		fmt.Println("Stworzono CSV Salda: ", time.Now())
+		jpk_wb.CreateCSVNumerRachunku(NumerRachunkuName)
+		fmt.Println("Zapisano CSV NumerRachunku: ", time.Now())
+		jpk_wb.CreateCSVNaglowek(NaglowekName)
+		fmt.Println("Zapisano CSV Naglowek: ", time.Now())
+		fmt.Println("Liczba faktur to: ", test)
+		fmt.Println("Liczba wierszy faktur to: ", len(jpk_fa.FakturaWiersz))
+		//exportCSV(csvFakturaWiersz, "JPK_FA_FakturaWiersz.txt")
+		jpk_wb.CreateCSVWyciagCtrl(WyciagCtrlName)
+		fmt.Println("Stworzono sumy kontrolne", time.Now())
+
+		end := time.Now()
+		fmt.Println("Start: ", st, "\nEnd: ", end)
+		fmt.Println("Wszystko trwało: ", time.Since(st))
+
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(wbCmd)
+}
---
+++ utils/wb.go
@@ -0,0 +1,164 @@
+package utils
+
+import "strings"
+
+// JPK_VAT cały nosiciel JPK to do niego wkładamy zprasowany plik
+type JPK_WB struct {
+	Naglowek      NaglowekWB     `xml:"Naglowek"`
+	Podmiot1      PodmiotWB      `xml:"Podmiot1"`
+	NumerRachunku string         `xml:"NumerRachunku"`
+	Salda         Salda          `xml:"Salda"`
+	WyciagWiersz  []WyciagWiersz `xml:"WyciagWiersz"`
+	WyciagCtrl    WyciagCtrl     `xml:"WyciagCtrl"`
+}
+
+type NaglowekWB struct {
+	KodFormularza      string `xml:"KodFormularza"`
+	WariantFormularza  string `xml:"WariantFormularza"`
+	CelZlozenia        string `xml:"CelZlozenia"`
+	DataWytworzeniaJPK string `xml:"DataWytworzeniaJPK"`
+	DataOd             string `xml:"DataOd"`
+	DataDo             string `xml:"DataDo"`
+	DomyslnyKodWaluty  string `xml:"DomyslnyKodWaluty"`
+	KodUrzedu          string `xml:"KodUrzedu"`
+}
+
+type PodmiotWB struct {
+	IndetyfikatorPodmiotu string `xml:"IdentyfikatorPodmiotu"`
+	AdresPodmiotu         string `xml:"AdresPodmiotu"`
+}
+
+type Salda struct {
+	SaldoPoczatkowe string `xml:"SaldoPoczatkowe"`
+	SaldoKoncowe    string `xml:"SaldoKoncowe"`
+}
+
+type WyciagWiersz struct {
+	NumerWiersza  string `xml:"NumerWiersza"`
+	DataOperacji  string `xml:"DataOperacji"`
+	NazwaPodmiotu string `xml:"NazwaPodmiotu"`
+	OpisOperacji  string `xml:"OpisOperacji"`
+	KwotaOperacji string `xml:"KwotaOperacji"`
+	SaldoOperacji string `xml:"SaldoOperacji"`
+}
+
+type WyciagCtrl struct {
+	LiczbaWierszy string `xml:"LiczbaWierszy"`
+	SumaObciazen  string `xml:"SumaObciazen"`
+	SumaUznan     string `xml:"SumaUznan"`
+}
+
+func (t JPK_WB) createRowSalda(i int) string {
+	str := ""
+
+	strSlice := []string{
+		t.Salda.SaldoPoczatkowe,
+		t.Salda.SaldoKoncowe}
+
+	strSlice = RemoveStringFromSliceOfString(strSlice, ";", "", -1)
+	joined := strings.Join(strSlice, ";")
+	str = str + joined + "\n"
+
+	return str
+}
+
+func (t JPK_WB) createRowWyciagWiersz(i int) string {
+	str := ""
+
+	strSlice := []string{
+		t.WyciagWiersz[i].NumerWiersza,
+		t.WyciagWiersz[i].DataOperacji,
+		t.WyciagWiersz[i].NazwaPodmiotu,
+		t.WyciagWiersz[i].OpisOperacji,
+		t.WyciagWiersz[i].KwotaOperacji,
+		t.WyciagWiersz[i].SaldoOperacji}
+
+	strSlice = RemoveStringFromSliceOfString(strSlice, ";", "", -1)
+	joined := strings.Join(strSlice, ";")
+	str = str + joined + "\n"
+
+	return str
+}
+
+func (t JPK_WB) LiczbaWierszyWyciagow() int {
+	return (len(t.WyciagWiersz))
+}
+
+func (t JPK_WB) CreateCSVFromRowsWyciagWiersz(buforSize int, filename string) {
+	str := ""
+	if buforSize == 0 {
+		buforSize = 200
+	}
+	for i := range t.WyciagWiersz {
+		rowPaste := t.createRowWyciagWiersz(i)
+		str = str + rowPaste
+		if i%200 == 0 {
+			//fmt.Println("Faktura: ", i)
+			WriteToCSV(str, filename)
+			str = ""
+		}
+	}
+	WriteToCSV(str, filename)
+}
+
+func (t JPK_WB) CreateCSVWyciagCtrl(filename string) {
+	str := ""
+
+	strSlice := []string{
+		t.WyciagCtrl.LiczbaWierszy,
+		t.WyciagCtrl.SumaObciazen,
+		t.WyciagCtrl.SumaUznan}
+
+	strSlice = RemoveStringFromSliceOfString(strSlice, ";", "", -1)
+	joined := strings.Join(strSlice, ";")
+	str = str + joined + "\n"
+
+	WriteToCSV(str, filename)
+}
+
+func (t JPK_WB) CreateCSVSalda(filename string) {
+	str := ""
+
+	strSlice := []string{
+		t.Salda.SaldoPoczatkowe,
+		t.Salda.SaldoKoncowe}
+
+	strSlice = RemoveStringFromSliceOfString(strSlice, ";", "", -1)
+	joined := strings.Join(strSlice, ";")
+	str = str + joined + "\n"
+
+	WriteToCSV(str, filename)
+}
+
+func (t JPK_WB) CreateCSVNumerRachunku(filename string) {
+	str := ""
+	strSlice := []string{
+		t.NumerRachunku}
+
+	strSlice = RemoveStringFromSliceOfString(strSlice, ";", "", -1)
+	joined := strings.Join(strSlice, ";")
+	str = str + joined + "\n"
+
+	WriteToCSV(str, filename)
+
+}
+
+func (t JPK_WB) CreateCSVNaglowek(filename string) {
+	str := ""
+
+	strSlice := []string{
+		t.Naglowek.KodFormularza,
+		t.Naglowek.WariantFormularza,
+		t.Naglowek.CelZlozenia,
+		t.Naglowek.DataWytworzeniaJPK,
+		t.Naglowek.DataOd,
+		t.Naglowek.DataDo,
+		t.Naglowek.DomyslnyKodWaluty,
+		t.Naglowek.KodUrzedu}
+
+	strSlice = RemoveStringFromSliceOfString(strSlice, ";", "", -1)
+	joined := strings.Join(strSlice, ";")
+	str = str + joined + "\n"
+
+	WriteToCSV(str, filename)
+}
